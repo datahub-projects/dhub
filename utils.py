45aeea2015269ba123dfa5e129565f81e01405f9
danx0r <dan.miller@eye0.com>
2020-01-02 02:16:00 -0800

    gitignore + utils

---
+++ utils.py
@@ -0,0 +1,25 @@
+from run import run
+#
+# run a git command, capture the output
+#
+def git(cmd, show=False, debug=False):
+    if debug:
+        print ("--> git", cmd)
+    out, good = run(["git"] + cmd.split(), showoutput=show)
+    if not good:
+        err = "ERROR -- git command did not complete"
+        print (err, file=sys.stderr)
+        out += "\n\n" + err
+    return out, not good
+
+#
+# get full pathnames of active subdirectories
+#
+def get_subs():
+    out, err = git("submodule status")
+    subs = []
+    for r in out.split("\n"):
+        if r.strip():
+            sub = r.split()[1]
+            subs.append(sub)
+    return subs
