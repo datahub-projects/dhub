314c79bd6211152ce93b567f21fc9beee7f22199
danx0r <dan.miller@eye0.com>
2020-01-13 11:10:13 -0800

    color for test cmds

---
+++ _tests/test1.py
@@ -0,0 +1,29 @@
+import os
+from blessings import Terminal
+bless_term = Terminal()
+
+
+def print_blue(s, **kw):
+    print (bless_term.blue(s), **kw)
+
+
+def do(s):
+    print_blue(s)
+    os.system(s)
+
+do("rm -fr root* *bare my*")
+
+do("./clean")
+
+do("echo '~~~~~~~~~~~~~~~~~~~~~~~~~TEST 1~~~~~~~~~~~~~~~~~~~~~~~~~~~'")
+do("ver clone ./rootbare --name myroot --yes")
+do("ls -Rrlt myroot")
+os.chdir("myroot")
+do("ver branch b1 --yes --debug")
+do("ver branch --debug")
+do("cat 'To another end' > sub1/rooty_file2")
+do("ver sync")
+do("ver branch master --debug")
+do("ver branch --debug")
+os.chdir("sub1")
+do("git branch")
