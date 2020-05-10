1c310ed448166b00079bb5ae573aea7c05db9365
danx0r <dan.miller@eye0.com>
2020-05-10 12:55:14 -0700

    importname

--- dhub/dhub.py
+++ dhub/dhub.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 
     reqs_args = subparsers.add_parser('reqs')
     reqs_args.add_argument("--package")
-    reqs_args.add_argument("--import")
+    reqs_args.add_argument("--importname")
     reqs_args.add_argument("--file")
     reqs_args.add_argument("--date", default="3000-1-1")
 
@@ -34,3 +34,8 @@ if __name__ == "__main__":
                 pkg=pkg.split("==")[0]
                 version, date = get_pip.get_latest(pkg, b4)
                 print("{0: <40} #released {1}".format("{0}=={1}".format(pkg, version), date.ctime()))
+        elif args.importname:
+            print ("Coming soon: cv2 implies openCV")
+
+        else:
+            print ("Coming soon: use pipreqs to create requirements list from scratch")
