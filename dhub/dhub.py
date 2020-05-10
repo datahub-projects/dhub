2ea6abf14a71d44b976f001ede0baa15b45859e6
danx0r <dan.miller@eye0.com>
2020-05-09 20:41:12 -0700

    parse requirements.txt for righteous advance

--- dhub/dhub.py
+++ dhub/dhub.py
@@ -8,10 +8,9 @@ if __name__ == "__main__":
 
     reqs_args = subparsers.add_parser('reqs')
     reqs_args.add_argument("--_reqs_", default=True)
-    reqs_args.add_argument("--print", action="store_true")
-    reqs_args.add_argument("--force")
-    reqs_args.add_argument("--import")
     reqs_args.add_argument("--package")
+    reqs_args.add_argument("--import")
+    reqs_args.add_argument("--file")
     reqs_args.add_argument("--date", default="3000-1-1")
 
     args = parser.parse_args()
@@ -21,4 +20,17 @@ if __name__ == "__main__":
         print ("REQS", args, b4)
         if args.package:
             version, date = get_pip.get_latest(args.package, b4)
-            print ("{0}=={1}\nreleased {2}".format(args.package, version, date.ctime()))
+            print ("{0}=={1}   #released {2}".format(args.package, version, date.ctime()))
+        elif args.file:
+            f=open(args.file)
+            for row in f.readlines():
+                row=row.strip()
+                if not row:
+                    continue
+                if row[0]=='#':
+                    continue
+                if row[0]=='-':
+                    print ("Skipping", row)
+                row=row.split("==")[0]
+                version, date = get_pip.get_latest(row, b4)
+                print("{0}=={1}   #released {2}".format(args.package, version, date.ctime()))
