8a8083a678678888c6e18bb00d47a80dc54e1ef8
danx0r <dan.miller@eye0.com>
2020-05-09 20:44:31 -0700

    corrected

--- dhub/dhub.py
+++ dhub/dhub.py
@@ -17,20 +17,20 @@ if __name__ == "__main__":
 
     if hasattr(args, "_reqs_"):
         b4 = parsedate(args.date)
-        print ("REQS", args, b4)
+        # print ("REQS", args, b4)
         if args.package:
             version, date = get_pip.get_latest(args.package, b4)
             print ("{0}=={1}   #released {2}".format(args.package, version, date.ctime()))
         elif args.file:
             f=open(args.file)
-            for row in f.readlines():
-                row=row.strip()
-                if not row:
+            for pkg in f.readlines():
+                pkg=pkg.strip()
+                if not pkg:
                     continue
-                if row[0]=='#':
+                if pkg[0]=='#':
                     continue
-                if row[0]=='-':
-                    print ("Skipping", row)
-                row=row.split("==")[0]
-                version, date = get_pip.get_latest(row, b4)
-                print("{0}=={1}   #released {2}".format(args.package, version, date.ctime()))
+                if pkg[0]=='-':
+                    print ("Skipping", pkg)
+                pkg=pkg.split("==")[0]
+                version, date = get_pip.get_latest(pkg, b4)
+                print("{0}=={1}   #released {2}".format(pkg, version, date.ctime()))
