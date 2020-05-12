983d30e7756d61f9d80a6a50f6c32d6ee713fad4
danx0r <dan.miller@eye0.com>
2020-05-11 20:15:15 -0700

    deps

--- setup.py
+++ setup.py
@@ -5,5 +5,10 @@ setup(
     packages=find_packages(),
     python_requires='>=3',
     scripts=['bin/dhub'],
+    install_requires=[
+        "requests == 2.23.0",
+        "blessings == 1.7",
+        "python_dateutil == 2.8.1",
+    ],
     include_package_data=True
 )
