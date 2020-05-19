6eae225bf2e3bff94a3f87a9cff5c1683cc6651c
danx0r <dan.miller@eye0.com>
2020-05-19 13:23:20 -0700

    rm unused

--- dhub/runrun.py
+++ dhub/runrun.py
@@ -109,15 +109,10 @@ from queue import Queue, Empty
 import time
 
 
-# def getabit(o, q):
-#     for c in iter(lambda:o.read(1), b''):
-#         q.put(c)
-#     o.close()
-
-
 def getabit(o, q):
     def getchar():
         return o.read(1)
+
     for c in iter(getchar, b''):
         q.put(c)
     o.close()
@@ -148,45 +143,6 @@ def test_func(s):
     # print ("RESULT:", N)
     # print ("----------/PARSE------------")
     return "BOOM " * N
-
-
-def run_interactive(cmd, input_func):
-    pobj = sp.Popen(cmd, stdin=sp.PIPE, stdout=sp.PIPE, shell=True)
-    q = Queue()
-    t = Thread(target=getabit, args=(pobj.stdout, q))
-    t.daemon = True
-    t.start()
-    in_dat=""
-    while True:
-        # print('Sleep for 1 second...')
-        time.sleep(.4)#to ensure that the data will be processed completely
-        o_dat = getdata(q).decode()
-        if not o_dat:
-            break
-        # print("~%s~" % o_dat)
-        if o_dat.find(in_dat+"\n")==0:
-            o_dat=o_dat[len(in_dat)+1:]
-        # print ("LEN:", len(in_dat))
-        if o_dat[-1:]=="\n":
-            end = "\n"
-            o_dat = o_dat[:-1]
-        else:
-            end = ''
-        rows = o_dat.split("\n")
-        # print("=>%s<="%rows)
-        rows = "$ " + "\n$ ".join(rows)
-        print(rows, end=end)
-        if t.isAlive():
-            in_dat = input_func(o_dat)
-            if end:
-                print ("> %s"%in_dat)
-            else:
-                print (in_dat)
-            pobj.stdin.write(bytes(in_dat, 'utf-8'))
-            pobj.stdin.write(b'\n')
-            pobj.stdin.flush()
-    time.sleep(1)
-    pobj.wait(15)
 
 
 class runner:
