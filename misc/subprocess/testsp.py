fa00b31acce27fef18f311022845d5417d11e458
danx0r <dan.miller@eye0.com>
2020-05-18 20:45:10 -0700

    line-by-line

--- misc/subprocess/testsp.py
+++ misc/subprocess/testsp.py
@@ -1,11 +1,16 @@
 import subprocess as sp
 from threading import Thread
-from queue import Queue,Empty
+from queue import Queue, Empty
 import time
 import random
 
-def getabit(o,q):
-    for c in iter(lambda:o.read(1),b''):
+# def getabit(o, q):
+#     for c in iter(lambda:o.read(1), b''):
+#         q.put(c)
+#     o.close()
+
+def getline(o, q):
+    for c in iter(lambda:o.readline(), b''):
         q.put(c)
     o.close()
 
@@ -20,9 +25,9 @@ def getdata(q):
             r += c
     return r
 
-pobj = sp.Popen('python3 called.py',stdin=sp.PIPE,stdout=sp.PIPE,shell=True)
+pobj = sp.Popen('python3 called.py', stdin=sp.PIPE, stdout=sp.PIPE, shell=True)
 q = Queue()
-t = Thread(target=getabit,args=(pobj.stdout,q))
+t = Thread(target=getline, args=(pobj.stdout, q))
 t.daemon = True
 t.start()
 
@@ -33,6 +38,6 @@ while True:
     if not t.isAlive():
         break
     in_dat = "BOOM" if random.random()>.5 else "BOOM BOOM"
-    pobj.stdin.write(bytes(in_dat,'utf-8'))
+    pobj.stdin.write(bytes(in_dat, 'utf-8'))
     pobj.stdin.write(b'\n')
     pobj.stdin.flush()
