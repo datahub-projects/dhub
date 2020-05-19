135b9cdf691a0d3d36a469d5a751d7de284f092b
danx0r <dan.miller@eye0.com>
2020-05-18 18:14:25 -0700

    weird way to pexpect

--- misc/pexpect/called.py
+++ misc/pexpect/called.py
@@ -1,6 +1,7 @@
 import random
 n=random.randint(0,7)
-print ("what is %s * 2? " % n, end='')
+print ("A\nBB\nCCC\n")
+print ("what the hell is %s * 2? " % n, end='')
 x=input("type answer:\n")
 if int(x)==n*2:
     print ("Correct")
--- misc/pexpect/test.py
+++ misc/pexpect/test.py
@@ -2,14 +2,18 @@ import sys, pexpect
 child = pexpect.spawn("python called.py")
 child.logfile_read = sys.stdout.buffer
 
-child.expect('\n')
+try:
+    child.expect('xxxxx', timeout=3)
+except:
+    print ("TIME")
 p=(child.before.decode('utf8'))
 print ("  Some computer program is asking me:", p)
 
-p=p.replace('?',"").split()
-a=int(p[2])
-b=int(p[4])
-c=a*b
+# p=p.replace('?',"").split()
+# a=int(p[2])
+# b=int(p[4])
+# c=a*b
 
-child.sendline(str(c))
+child.sendline("11")
 child.expect(pexpect.EOF)
+child.wait()
